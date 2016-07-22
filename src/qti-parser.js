2b56f051d169dfac2d881aa687f9f1df7d8fa663
Nick <nick.benoit14@gmail.com>
2016-07-22 15:06:07 -0600

    build process

--- src/qti-parser.js
+++ src/qti-parser.js
@@ -1,7 +1,13 @@
+import Tokenizer from './tokenizer';
+
 const qtiParser = {
   greet() {
     return 'hello';
-  }
+  },
+  Tokenizer
+
 };
 
+
+console.log("Howdy Do");
 export default qtiParser;
---
+++ src/tokenizer.js
@@ -0,0 +1,8 @@
+// require("jsdom").env("", function(err, window) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//
+//     var $ = require("jquery")(window);
+// });
