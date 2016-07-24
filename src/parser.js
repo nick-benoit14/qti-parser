b72662c73379fae5e76fbeecd90d5691f5422beb
Nick <nick.benoit14@gmail.com>
2016-07-24 10:07:17 -0600

    add _parse_tag

--- src/parser.js
+++ src/parser.js
@@ -3,7 +3,7 @@ import Tokenizer from './tokenizer';
 
 export const Grammar = {
   questestinterop: 'questestinterop',
-  text: '#text',
+  _text: '_text',
   assessment: 'assessment',
   qtimetadata: 'qtimetadata',
   section: 'section',
@@ -35,76 +35,24 @@ export default class Parser{
   }
 
   parse_atom(current){
-    switch (current.nodeName) {
-      case Grammar.questestinterop:
-        return this.parse_questestinterop(current);
-        break;
-      case Grammar.text:
-        break;
-      case Grammar.assessment:
-        return this.parse_assessment(current);
-        break;
-      case Grammar.qtimetadata:
-        break;
-      case Grammar.section:
-        return this.parse_section(current);
-        break;
-      case Grammar.item:
-        return this.parse_item(current);
-        break;
-      case Grammar.itemmetadata:
-        break;
-      case Grammar.presentation:
-        break;
-      case Grammar.resprocessing:
-        break;
-      default:
-        debugger;
-        throw new Error(`${this.current.type} is not yet supported!`);
-    }
+    return this._parse_tag(current);
   }
 
   parse(){
     this.parse_topLevel((current) => this.parse_atom(current));
   }
 
-  parse_questestinterop(current){
-    var tokens = new Tokenizer(current.childNodes);
-    var questestinterop = tokens.each((val) => this.parse_atom(val));
-    questestinterop.type = Grammar.questestinterop;
-
-    return questestinterop;
-  }
-
-  parse_assessment(current){
-    var tokens = new Tokenizer(current.childNodes);
-    var assessment = tokens.each((val) => this.parse_atom(val));
-    assessment.type = Grammar.assessment;
-
-    return assessment;
-  }
-
-  parse_section(current){
-    var tokens = new Tokenizer(current.childNodes);
-    var section = tokens.each((val) => this.parse_atom(val));
-    section.type = Grammar.section;
-
-    return section;
-  }
-
-  parse_item(current){
+  _parse_tag(current){
     var tokens = new Tokenizer(current.childNodes);
     var item = tokens.each((val) => this.parse_atom(val));
-    item.type = Grammar.item;
+    var name = _.replace(current.nodeName, '#', '_');
+    item.type = Grammar[name];
+    if(!item.type){
+      // debugger;
+      // throw new Error(`${current.nodeName} is not yet supported!`);
+      console.error(`${current.nodeName} is not yet supported!`);
+    }
 
     return item;
   }
-
-
-  // parse_presentation(current){}
-  // parse_material(){}
-  // parse_mattext(){}
-  // parse_response_lid(){}
-  // parse_render_choice(){}
-  // parse_response_label(){}
 };
